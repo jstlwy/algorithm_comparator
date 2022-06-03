0979843a54a70c90627d8ed7f6f3add10912d2fc
jstlwy <jlbsw20@gmail.com>
2022-06-03 16:13:19 -0700

    Restrict variable scope

--- arraysort.c
+++ arraysort.c
@@ -3,28 +3,25 @@
 
 void selection_sort(int *array, const int array_size)
 {
-	int min;
-	int min_index;
 	for (int i = 0; i < array_size; i++) {
-		min = i;
+		int min = i;
 		for (int j = i + 1; j < array_size; j++) {
-			if (array[j] < min) {
-				min = array[j];
-				min_index = j;
+			if (array[j] < array[min]) {
+				min = j;
 			}
 		}
-		array[min_index] = array[i];
-		array[i] = min;
+		int temp = array[i];
+		array[i] = array[min];
+		array[min] = temp;
 	}
 }
 
 
 void insertion_sort(int *array, const int array_size)
 {
-	int j;
 	for (int i = 1; i < array_size; i++) {
 		int key = array[i];
-		j = i - 1;
+		int j = i - 1;
 		while (j >= 0 && array[j] > key) {
 			array[j + 1] = array[j];
 			j--;
